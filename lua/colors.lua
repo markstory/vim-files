14a9b24202f3e88452f70ad335b924e4fb4b482c
Mark Story <mark@mark-story.com>
2022-02-28 14:02:52 -0500

    Increase contrast

--- lua/colors.lua
+++ lua/colors.lua
@@ -7,10 +7,10 @@ vim.g.termguicolors = true
 -- Theme
 --vim.cmd[[color base16_one_light]]
 vim.g.tokyonight_style = "day"
-vim.g.tokyonight_day_brightness = 0.3
+vim.g.tokyonight_day_brightness = 0.2
 --vim.g.tokyonight_transparent = 1
 vim.g.tokyonight_lualine_bold = 1
 vim.g.tokyonight_sidebars = { "fern" }
---vim.g.tokyonight_dark_sidebar = true
+vim.g.tokyonight_dark_sidebar = true
 
 vim.cmd[[colorscheme tokyonight]]
