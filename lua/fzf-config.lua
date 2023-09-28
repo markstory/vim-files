0fb20b1bab427a8e1f8a65b2f00ac67c910494e4
Mark Story <mark@mark-story.com>
2023-09-28 10:45:53 -0700

    Fix fzf not using ignore list

--- lua/fzf-config.lua
+++ lua/fzf-config.lua
@@ -6,7 +6,6 @@ require('fzf-lua').setup({
     col = 0.3,
   },
   files = {
-    cmd = "find -L .",
     find_opts = [[-type f -not -path '*.git*' -not -path '*node_modules*' -not -path '*vendor*' -not -path '*.venv*' -not -path '*.env*' -not -path '*.mypy_cache*' -not -path '*.pytest_cache*' -not -path '*.mypy_cache*']],
     prompt = 'Files❯ ',
     multiprocess = true,
