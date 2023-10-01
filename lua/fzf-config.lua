fe63321415349283df6e403dbac3dfe7253a9c0d
Mark Story <mark@mark-story.com>
2023-10-01 00:19:38 -0700

    Fix .github not being reachable

--- lua/fzf-config.lua
+++ lua/fzf-config.lua
@@ -6,7 +6,7 @@ require('fzf-lua').setup({
     col = 0.3,
   },
   files = {
-    find_opts = [[-type f -not -path '*.git*' -not -path '*node_modules*' -not -path '*vendor*' -not -path '*.venv*' -not -path '*.env*' -not -path '*.mypy_cache*' -not -path '*.pytest_cache*' -not -path '*.mypy_cache*']],
+    find_opts = [[-type f -not -path '*.git/objects*' -not -path '*node_modules*' -not -path '*vendor*' -not -path '*.venv*' -not -path '*.env*' -not -path '*.pytest_cache*' -not -path '*.mypy_cache*']],
     prompt = 'Files❯ ',
     multiprocess = true,
     file_icons = true,
